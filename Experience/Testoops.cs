c9aa78728556873cf640462049e9ba1260cab951
2001monika <105417181+2001monika@users.noreply.github.com>
2022-08-26 17:31:49 +0530

    Add files via upload

---
+++ Experience/Testoops.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assingments
+{
+    internal class Testoops
+    {
+        public static void Main()
+        {
+            oops e = new oops(22855, "moni", new DateOnly(2020, 8, 1));
+            Console.WriteLine($"year of experience{e.GetYearsofExp()}");
+
+        }
+    }
+}
