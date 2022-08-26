87822e97109155c745f6920148d3bcec0ff0ed9f
Monika <monika.gmony@gmail.com>
2022-08-26 18:26:59 +0530

    experience of emp

--- Batch1-DET-2022/Testarea_of_circle.cs
+++ Batch1-DET-2022/Testarea_of_circle.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Batch1_DET_2022
-{
-    internal class Testarea_of_circle
-    {
-        public static void Main()
-        {
-            area_of_circle a = new area_of_circle(2);
-            Console.WriteLine($"area of circle is{a.CalculateArea()}");
-        }
-    }
-}
+//namespace Batch1_DET_2022
+//{
+//    internal class Testarea_of_circle
+//    {
+//        public static void Main()
+//        {
+//            area_of_circle a = new area_of_circle(2);
+//            Console.WriteLine($"area of circle is{a.CalculateArea()}");
+//        }
+//    }
+//}
